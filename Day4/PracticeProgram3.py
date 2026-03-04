0e13a00ce5a868bbba53a7689c4d3e3fd18ea551
AshishMulani <mulaniashish16@gmail.com>
2026-03-04 21:48:00 +0530

    practice program

--- Day4/PracticeProgram3.py
+++ Day4/PracticeProgram3.py
@@ -59,27 +59,50 @@
 # display_greeting('Good Morning')
 
 # Q. 4. Create series of 'n' prime numbers and display first 10
+n=int(input('Enter series size : '))
+series=[i for i in range(1,n+1)]
+print(series)
 
+prime=list(filter(lambda v:v>1 and all( v%j!=0 for j in range(2,v)),series))
+print(prime[:10])
 
 
 # Q.5
-emp_data = {'Amol': ['C', 'C++', 'Java'], 'Aditya': ['Angular', 'Java'],
-           'Aditi': ['Python', 'PHP', 'Database']}
+# emp_data = {'Amol': ['C', 'C++', 'Java'], 'Aditya': ['Angular', 'Java'],
+#            'Aditi': ['Python', 'PHP', 'Database']}
+#
+# # 1. Find employees that know 'python'
+# l1=dict(filter(lambda item:'python'in [i.lower() for i in item[1]],emp_data.items()))
+# print(l1)
 
-# 1. Find employees that know 'python'
-l1=dict(filter(lambda item:'Python'in item[1],emp_data.items()))
-print(l1)
-
-# 2. Add a new skill - 'test' in skillset of all employees
+# # 2. Add a new skill - 'test' in skillset of all employees
+# l2=dict(map(lambda item:(item[0], item[1]+['test']),emp_data.items()))
+# print(l2)
+# #or
+# l2={k:(lambda v:v+['test'])(v) for k,v in emp_data.items()}
+# print(l2)
 
 # 3. Sort employees by skills
-# for the given dictionary of employees
+# l3=dict(sorted(emp_data.items(),key=lambda item:item[1]))
+# print(l3)
 
-# Q.6
-# Following data displays min/max/average temp for cities
-# weather= [{'Mumbai' : [28, 30, 32]},.....]
+
+# # Q.6
+# # Following data displays min/max/average temp for cities
+# weather= [{'Mumbai' : [28, 30, 32]},
+#           {'Pune':[34,35,26]},
+#           {'Nashik': [24, 25, 27]}]
+# #
+# # 1. Print the weather data
+# print(weather)
 #
-# 1. Print the weather data
-# 2. Print the city with maximum/min temp
-# 3. Print all the cities that expereince min temp more than 30 degree
-# 4. Create a dictionary to print 'City':'Ave temp'
+# # 2. Print the city with maximum/min temp
+# print(max(weather,key=lambda item: max(list(item.values())[0])))
+# print(min(weather,key=lambda item: min(list(item.values())[0])))
+#
+# # 3. Print all the cities that expereince min temp more than 30 degree
+# print(list(filter(lambda item: any(item>30 for item in list(item.values())[0]),weather)))
+#
+# # 4. Create a dictionary to print 'City':'Ave temp'
+# avg_temp=dict(map(lambda v: (list(v.keys())[0],sum(list(v.values())[0])/len(list(v.values())[0])),weather))
+# print(avg_temp)
