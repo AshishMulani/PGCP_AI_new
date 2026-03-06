93f9162ba956e3d1504b78df4ba2c743f8823461
PGCP-AI <mulaniashish16@gmail.com>
2026-03-06 20:14:15 +0530

    Day6 new

--- Day6/Assignment2_User.py
+++ Day6/Assignment2_User.py
@@ -9,10 +9,10 @@ from Assignment2_Accounts import(Account,SavingsAccount,CurrentAccount)
 from Assignment2_Transaction import Transaction
 
 class User:
-    def __init__(self,acc):
-        self._acc=acc
+    def __init__(self,account):
+        self._account=account
         self._transaction=Transaction()
-        # self._statement=[]
+        self._statement=[]
 
     def menu(self):
         print('-------- Bank Menu --------')
@@ -26,16 +26,19 @@ class User:
 
             try:
                 if choice==1:
-                    amt=int(input('Enter amount to deposit: '))
-                    bal=self._transaction.deposit_to_account(self._acc,amt)
-                    msg=f'Deposited amount: {amt} | Balance: {bal}'
-                    print(msg)
+                    bal = transaction.deposit_to_account(sap)
+
+
+                    # msg=f'Deposited amount: {amt} | Balance: {bal}'
+                    # print(msg)
+                    # self._statement.append(msg)
 
                 elif choice == 2:
                     amt = int(input('Enter amount to withdraw: '))
-                    bal = self._transaction.withdraw_from_account(self._acc, amt)
+                    bal = self._transaction.withdraw_from_account(self._account, amt)
                     msg = f'Withdraw amount: {amt} | Balance: {bal}'
                     print(msg)
+                    self._statement.append(msg)
 
                 elif choice==3:
                     break
@@ -45,6 +48,12 @@ class User:
 
             except Exception as e:
                 print(e)
+        self.display_statement()
 
-sap=SavingsAccount(101,'Pranita',80000,'personal')
+
+    def display_statement(self):
+        for s in self._statement:
+            print(s)
+
+sap=SavingsAccount(101,'Pranita',80000,'corporate')
 User(sap).menu()
