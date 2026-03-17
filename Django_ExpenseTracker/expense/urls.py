7a4c2a9f5c66ef8f76e196d4d959cf66186015d4
PGCP-AI <mulaniashish16@gmail.com>
2026-03-17 15:28:45 +0530

    pandas

---
+++ Django_ExpenseTracker/expense/urls.py
@@ -0,0 +1,6 @@
+# urlpatterns = [
+#     path('', views.home, name='book-home'),
+#     path('about/', views.about, name='book-about'),
+#     path('book/<int:book_id>/', views.book_details, name='book-details'),
+#     path('register/', views.register, name='book-register')
+# ]
