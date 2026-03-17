442600f5414b01f3041ccd46ed8e0c8df5f4bf44
PGCP-AI <mulaniashish16@gmail.com>
2026-03-17 12:08:44 +0530

    pandas

--- Django_BookStore/book/views.py
+++ Django_BookStore/book/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render
 
+from .models import Book
+
+
+# books=[{
+#         'title':'python 3',
+#         'author':'abc',
+#         'price':900
+#         },
+#        {
+#         'title': 'Data science',
+#         'author': 'pqr',
+#         'price': 1200
+#        },
+#        {
+#         'title': 'java',
+#         'author': 'xyz',
+#         'price': 1000
+#        }
+#        ]
+
+
 def home(request):
-    return render(request, 'book/index.html')
+    books=Book.objects.all()
+    context={'books':books}   #dict
+    return render(request, 'book/home.html',context=context)
 
 def about(request):
-    return render(request, 'book/about.html')
+    return render(request, 'book/about.html',{'title':'About'})
+
+#def about(request):
